ef95d53d3f63cee0cdb3891fc34a7294a7aae022
robert <robert.k25@gmail.com>
2016-12-19 11:53:03 -0500

    adds database name to connections and configures model to use local mongodb, additionally adds api skeleton for FarmersMarket

---
+++ localGrub/api/controllers/FarmersMarketController.js
@@ -0,0 +1,11 @@
+/**
+ * FarmersMarketController
+ *
+ * @description :: Server-side logic for managing farmersmarkets
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
