fd8dae1a8a2f34136aa4c141fba73dcd1145d212
robert <robert.k25@gmail.com>
2016-12-21 11:54:13 -0500

    adds a search view and factory for ngResource

--- localGrub-frontend/js/app.js
+++ localGrub-frontend/js/app.js
@@ -1,8 +1,30 @@
-angular.module("localGrub", [])
-  .controller("SearchController", [SearchControllerFunction])
+angular.module("localGrub", ["ui.router", "ngResource"])
+  .config(["$stateProvider", Router])
+  .factory("SearchFactory", ["$resource", SearchFactoryFunction])
+  .controller("SearchController", ["SearchFactory", SearchControllerFunction])
 
 
 
-function SearchControllerFunction() {
-  console.log("hello world")
+function Router($stateProvider) {
+  console.log("Router working")
+  //search state
+  $stateProvider
+  .state("search", {
+      url: "/",
+      templateUrl: "js/ng-views/search.html",
+      controller: "SearchController",
+      controllerAs: "vm"
+    })
+    //show state
+}
+
+function SearchFactoryFunction($resource) {
+  return $resource("http://localhost:1337/zipcode/:zip", {}, {
+    update: {method: "PUT"}
+  })
+}
+
+function SearchControllerFunction(SearchFactory) {
+  this.results = SearchFactory.get({zip:"11222"})
+  console.log(this.results)
 }
